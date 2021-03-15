a0002335cc38be222f74f3178610270bf0315591
JoBenEtuk <JoBenEtuk@users.noreply.github.com>
2021-03-15 16:15:22 +0100

    dependency array

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
   useEffect(() => {
     getRecipes()
     console.log(recipes)
-  }, [query])
+  })
 
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
